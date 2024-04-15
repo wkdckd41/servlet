ea429369d25f11d5d4f2ac9bc85eba6a838a95ff
wkdckd41 <wkdckd41@gmail.com>
2024-04-15 23:55:22 +0900

    MVC 프레임워크 만들기 - View 분리-v2

---
+++ src/main/java/study/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,29 @@
+package study.servlet.web.frontcontroller.v2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import study.servlet.domain.member.Member;
+import study.servlet.domain.member.MemberRepository;
+import study.servlet.web.frontcontroller.MyView;
+import study.servlet.web.frontcontroller.v2.ControllerV2;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        request.setAttribute("member", member);
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
+}
